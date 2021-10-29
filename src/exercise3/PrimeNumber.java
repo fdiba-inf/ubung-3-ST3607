d6016288181bb2b8952f627c26e553ec7c931088
ST3607 <>
2021-10-29 19:18:54 +0000

    Files

---
+++ src/exercise3/PrimeNumber.java
@@ -0,0 +1,20 @@
+package exercise3;
+import java.util.Scanner;
+public class PrimeNumber {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int number = input.nextInt();
+    int devider = 2;
+    boolean prime = true;
+
+    while (devider < number) {
+      if (number % devider == 0) {
+        prime = false;
+        break;
+      }
+      devider++;
+    }
+    System.out.println("Prime number: " + prime);
+  }
+}
+    
